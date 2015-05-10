a08954305a89f35eeb38b1e6bf8eddddcb9a6337
Aidan Holm <aidanholm@gmail.com>
2015-05-10 16:00:21 +0800

    Add Solutions 11--16

--- 99_questions/1_to_20.hs
+++ 99_questions/1_to_20.hs
@@ -65,3 +65,50 @@ pack list = fst $ rpack ([],list)
 
 {-encode :: (Num a) => [a] -> [(n,a)]-}
 encode str = map (\x -> (length x, head x)) $ pack str
+
+{-Problem 11-}
+
+data Run a = Multiple Int a | Single a deriving (Show)
+encodeModified str = map (\x -> if fst x == 1 then (Single $ snd x) else (Multiple (fst x) (snd x))) $ encode str
+
+{-Problem 12-}
+
+decodeModified a = foldl (++) [] $ map f a
+    where f (Single a) = [a]
+          f (Multiple n a) = take n $ repeat a
+
+{-Problem 13-}
+
+runCount :: Run a -> Int
+runCount (Single _) = 1
+runCount (Multiple n _) = n
+
+runItem :: Run a -> a
+runItem (Single i) = i
+runItem (Multiple _ i) = i
+
+encodeDirectHelper :: (Eq a) => ([Run a],[a]) -> ([Run a],[a])
+encodeDirectHelper (a,[]) = (a,[])
+encodeDirectHelper ([],(x:xs)) = encodeDirectHelper ([Single x],xs)
+encodeDirectHelper (p,(x:xs)) = if x == runItem (last p)
+                    then encodeDirectHelper ((init p) ++ [Multiple (1 + runCount (last p)) x], xs)
+                    else encodeDirectHelper (p ++ [Single x], xs)
+
+encodeDirect list = fst $ encodeDirectHelper ([],list)
+
+{-Problem 14-}
+
+dupli :: [a] -> [a]
+dupli list = repli list 2
+
+{-Problem 15-}
+
+repli :: [a] -> Int -> [a]
+repli list num = foldl (++) [] $ map (\x -> take num (repeat x)) list
+
+{-Problem 16-}
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery list num = helper list (num-1) num
+    where helper [] _ _ = []
+          helper list n num = if n == 0 then helper (tail list) (num-1) num else (head list) : helper (tail list) (n-1) num
